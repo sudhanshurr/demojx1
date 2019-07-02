1a56975244bf7e190d727fd94c3ce4bd209648b6
sudhanshu <sudhanshurr@gmail.com>
2019-07-02 16:12:35 +0530

    changes

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Hello, World from Kubernetes! <br>";
-echo "Hello Jenkins X! <br>";
+echo "Hello, World from Anish Gupta! <br>";
+echo "Finally X! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
 ?>
